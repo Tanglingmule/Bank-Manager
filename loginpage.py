fd1ce62c12899bb26c886a74611e38f9e7d0e0cb
Tanglingmule <131406616+Tanglingmule@users.noreply.github.com>
2023-11-08 16:20:12 +0000

    added entry box for username

--- loginpage.py
+++ loginpage.py
@@ -2,10 +2,24 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
+default_text = True
+def delete_text(event):
+  global default_text
+  if default_text:
+    username.delete(0, END)
+    default_text = False
+
+
 register_window = tk.Tk()
 register_window.title('Register Now!')
 register_window.attributes('-topmost', True)
 register_window.geometry('600x600')
+window_theme=ttk.Style(theme='cyborg')
+
+username = ttk.Entry(register_window, style='info.Tentry')
+username.pack(pady=50)
+username.insert(END, 'Username Here')
+username.bind("<Button-1>", delete_text)
 
 
 
