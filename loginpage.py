5908de8ef7e64b0d7412c68ffe5007c022e96451
Tanglingmule <131406616+Tanglingmule@users.noreply.github.com>
2023-11-08 12:36:12 +0000

    Added tk and ttk and makes main register window

--- loginpage.py
+++ loginpage.py
@@ -1,1 +1,18 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+register_window = tk.Tk()
+register_window.title('Register Now!')
+register_window.attributes('-topmost', True)
+register_window.geometry('600x600')
+
+
+
+
+
+
+
+
+register_window.mainloop()
 
