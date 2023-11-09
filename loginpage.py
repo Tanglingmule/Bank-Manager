f08154e2c3ce88d6fad5091acb7d6c2731f9379d
Tanglingmule <131406616+Tanglingmule@users.noreply.github.com>
2023-11-09 14:27:17 +0000

    Added functionality

--- loginpage.py
+++ loginpage.py
@@ -7,30 +7,37 @@ import pandas as pd
 def check_details(username, password, recovery):
   if not recovery:
     if not username:
-      Messagebox.show_error(message='No username', title='Invalid', parent=None, alert=True)
+      Messagebox.show_error(message = 'No username', title = 'Invalid', parent = None, alert = True)
+    elif not password:
+      Messagebox.show_error(message = 'No password', title = 'Invalid', parent= None, alert = True)
+  return True
+
+def empty_entries():
+    username.delete(0,'end')
+    password.delete(0, 'end')
+    recovery.delete(0, 'end')
 
 
-default_text = True
-def delete_text_username(event):
-  global default_text
-  if default_text:
-    username.delete(0, END)
-    default_text = False
-    default_text = True
+def username_erase(event=None):
+    if username.get() == username_placeholder:
+        username.delete(0,'end')
+def username_add(event=None):
+    if username.get() == '':
+        username.insert(0,username_placeholder)
 
-def delete_text_password(event):
-  global default_text
-  if default_text:
-    password.delete(0, END)
-    default_text = False
-    default_text = True
+def password_erase(event=None):
+    if password.get() == password_placeholder:
+        password.delete(0,'end')
+def password_add(event=None):
+    if password.get() == '':
+        password.insert(0,password_placeholder)
 
-def delete_text_recovery(event):
-  global default_text
-  if default_text:
-    recovery.delete(0, END)
-    default_text = False
-    default_text = True
+def recovery_erase(event=None):
+    if recovery.get() == recovery_placeholder:
+        recovery.delete(0,'end')
+def recovery_add(event=None):
+    if recovery.get() == '':
+        recovery.insert(0,recovery_placeholder)
 
 register_window = tk.Tk()
 register_window.title('Login Now!')
@@ -42,21 +49,25 @@ username_placeholder='Username here'
 username = ttk.Entry(register_window, style='info.Tentry')
 username.pack(pady=5)
 username.insert(END, username_placeholder)
-username.bind("<Button-1>", delete_text_username)
+username.bind('<FocusIn>',username_erase)
+username.bind('<FocusOut>',username_add)
+
 
 password_placeholder='Password here'
 password = ttk.Entry(register_window, style='info.Tentry')
 password.pack(pady=5)
 password.insert(END, password_placeholder)
-password.bind('<Button-1>', delete_text_password)
+password.bind('<FocusIn>', password_erase)
+password.bind('<FocusOut>', password_add)
 
 recovery_placeholder='Recovery Key here (OPTIONAL)'
 recovery= ttk.Entry(register_window, style='info.Tentry')
 recovery.pack(pady=5)
 recovery.insert(END, recovery_placeholder)
-recovery.bind('<Button-1>', delete_text_recovery)
+recovery.bind('<FocusIn>', recovery_erase)
+recovery.bind('<FocusOut>', recovery_add)
 
-login_button = ttk.Button(register_window, text='Click To Login', style='info.Tbutton', command=check_details('','',''))
+login_button = ttk.Button(register_window, text='Click To Login', style='info.Tbutton', command= lambda: [check_details(username.get(),password.get(),recovery.get()),])
 login_button.pack(pady=5)
 
 
