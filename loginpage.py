8f8268e6bcc7d8372bd787ec98691a8db31b0a59
Tanglingmule <za1nkhans1a420@gmail.com>
2023-11-09 23:02:38 +0000

    Added remember me

--- loginpage.py
+++ loginpage.py
@@ -3,6 +3,8 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.dialogs import *
 import pandas as pd
+import socket
+import csv
 
 database_userdata= pd.read_csv("database_login.csv")
 print(database_userdata)
@@ -27,6 +29,7 @@ def check_details(username, password, recovery):
     else:
         if recovery not in database_userdata:
             Messagebox.show_error(message= 'Recovery Key does not exist', title= 'Invalid', parent= None, alert= True)
+    remember(var.get(), username)
     Messagebox.show_info(message= 'Logged In', alert= True, title= 'Logged In Successfully', parent= None)
     return True
 
@@ -37,6 +40,18 @@ def empty_entries(username_get, password_get, recovery_get):
         password.delete(0, 'end')
     if recovery_get == recovery_placeholder:
         recovery.delete(0, 'end')
+
+def remember(state, username):
+    if state:
+        hostname = socket.gethostname() #get the hostname
+        IP = socket.gethostbyname(hostname) #get ip from hostname
+
+        # Find the row with the matching username and update the 'IP' column
+        database_userdata.loc[database_userdata['Username'] == username, 'IP'] = IP
+
+        # Save the updated DataFrame to the CSV file
+        database_userdata.to_csv('database_login.csv', index=False)
+
 
 
 def username_erase(event=None):
@@ -60,14 +75,15 @@ def recovery_add(event=None):
     if recovery.get() == '':
         recovery.insert(0,recovery_placeholder)
 
-register_window = tk.Tk()
-register_window.title('Login Now!')
-register_window.attributes('-topmost', True)
-register_window.geometry('600x600')
+login_window = tk.Tk()
+login_window.title('Login Now!')
+login_window.attributes('-topmost', True)
+login_window.geometry('600x600')
 window_theme=ttk.Style(theme='darkly')
+var = tk.IntVar()
 
 username_placeholder='Username here'
-username = ttk.Entry(register_window, style='info.Tentry')
+username = ttk.Entry(login_window, style='primary.Tentry')
 username.pack(pady=5)
 username.insert(END, username_placeholder)
 username.bind('<FocusIn>',username_erase)
@@ -75,26 +91,28 @@ username.bind('<FocusOut>',username_add)
 
 
 password_placeholder='Password here'
-password = ttk.Entry(register_window, style='info.Tentry')
+password = ttk.Entry(login_window, style='primary.Tentry')
 password.pack(pady=5)
 password.insert(END, password_placeholder)
 password.bind('<FocusIn>', password_erase)
 password.bind('<FocusOut>', password_add)
 
 recovery_placeholder='Recovery Key here (OPTIONAL)'
-recovery= ttk.Entry(register_window, style='info.Tentry')
+recovery= ttk.Entry(login_window, style='primary.Tentry')
 recovery.pack(pady=5)
 recovery.insert(END, recovery_placeholder)
 recovery.bind('<FocusIn>', recovery_erase)
 recovery.bind('<FocusOut>', recovery_add)
 
-login_button = ttk.Button(register_window, text='Click To Login', style='info.Tbutton', command= lambda: [empty_entries(username.get(), password.get(), recovery.get()),check_details(username.get(),password.get(),recovery.get()),])
+login_button = ttk.Button(login_window, text='Click To Login', style='primary.Tbutton', command= lambda: [empty_entries(username.get(), password.get(), recovery.get()),check_details(username.get(),password.get(),recovery.get()),])
 login_button.pack(pady=5)
 
+remember_me = ttk.Checkbutton(login_window, text='Remember me', style='Roundtoggle.Toolbutton', variable= var)
+remember_me.pack(pady=3)
 
 
 
 
 
-register_window.mainloop()
+login_window.mainloop()
 
