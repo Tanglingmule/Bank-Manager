1b8dc172b1d6dc28bbc78037219ddac1571b02c8
Tanglingmule <za1nkhans1a420@gmail.com>
2023-11-15 11:19:35 +0000

    added stuff

--- registerpage.py
+++ registerpage.py
@@ -6,6 +6,7 @@ import pandas as pd
 import random
 import string
 
+
 def generate_recovery():
     length=15
     characters = string.ascii_letters + string.digits
@@ -16,26 +17,41 @@ def generate_recovery():
 
 
 def register_window():
-    register= tk.Tk()
+
+    register= tk.Toplevel()
     register.title('Register Now!')
     register.attributes('-topmost', True)
     register.geometry('600x600')
-    window_theme=ttk.Style(theme='darkly')
+    
 
     def username_erase(event=None):
         if username.get() == username_placeholder:
             username.delete(0,'end')
+    def username_add(event=None):
+        if username.get() == '':
+            username.insert(0,username_placeholder)
         
     def email_erase(event=None):
         if email.get() == email_placeholder:
             email.delete(0, END)
-    
+    def email_add(event=None):
+        if email.get() == email_placeholder:
+            email.delete(0, END)
     def password_erase(event=None):
         if password.get() == password_placeholder:
             password.delete(0, END)
+    def password_add(event=None):
+        if password.get() == '':
+            password.insert(0,password_placeholder)
 
-    #def email_add(event=None):
-        
+    def empty_entries(username_get, password_get, email_get):
+        if username_get == username_placeholder:
+            username.delete(0,'end')
+        if password_get == password_placeholder:
+            password.delete(0, 'end')
+        if email_get == email_placeholder:
+            email.delete(0, 'end')
+            
 
     email_placeholder='Email Here'
     email = ttk.Entry(register, style='primary.Tentry')
@@ -47,16 +63,19 @@ def register_window():
     username_placeholder='Username here'
     username = ttk.Entry(register, style='primary.Tentry')
     username.pack(pady=5)
-    username.insert(END, email_placeholder)
+    username.insert(END, username_placeholder)
     username.bind('<FocusIn>',username_erase)
     username.bind('<FocusOut>',username_add)
 
     password_placeholder='Password Here'
     password = ttk.Entry(register, style='primary.Tentry')
     password.pack(pady=5)
-    password.insert(END, email_placeholder)
+    password.insert(END, password_placeholder)
     password.bind('<FocusIn>',password_erase)
     password.bind('<FocusOut>', password_add)
+
+    register_button = ttk.Button(register_window, text='Click To Register', style='primary.Tbutton', command= lambda: [empty_entries(username.get(), password.get(), email.get()),check_details(username.get(),password.get(),email.get()),])
+    register_button.pack(pady=5)
 
     register.mainloop()
     
